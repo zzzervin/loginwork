590029cd2795927fdc1fa7b6d61e7e85f38d762a
Opilane TTHK <opilane@tthk.ee>
2021-11-23 09:59:45 +0200

    first commit

---
+++ KEK/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace KEK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //login:admin
+            //password: pass1234
+            
+            //deslare variablas
+            //assing value -
+            string login = "admin";
+            string password = "pass1234";
+
+            string userName, userPassword;
+
+            Console.WriteLine("enthe your login:");
+            userName = Console.ReadLine();
+            Console.WriteLine("enthe your password:");
+            userPassword = Console.ReadLine();
+
+            if (login==userName && password==userPassword)
+                Console.WriteLine("Welcome!");
+            else
+                Console.WriteLine("Oops!");
+
+        }
+    }
+}
